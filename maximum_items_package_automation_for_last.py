d01814a24f054cd07070855bfa07ec9139407f4a
TomerRabani <51020484+TomerRabani@users.noreply.github.com>
2021-09-10 13:16:57 -0700

    Create maximum_items_package_automation_for_last.py
    https://leetcode.com/discuss/interview-question/1018060/amazon-oa-sde-2021-hackerrank-packaging-automation

---
+++ maximum_items_package_automation_for_last.py
@@ -0,0 +1,26 @@
+#  https://leetcode.com/discuss/interview-question/1018060/amazon-oa-sde-2021-hackerrank-packaging-automation
+# Given a list of items in groups, perform certain operations in order to satisfy the constaints required by packaging automation.
+# The conditions are as follows:
+
+# The first group must contain 1 item only.
+# For all the other groups, the difference between the number of items in adjacent groups must NOT be greater than 1. In other words, for 1 <= i <= n, arr[i] - arr[i - 1] <= 1. To accomplish this, the following operations are available:
+# Rearrange the groups in any way.
+# Reduce any group to any number that is >= 1.
+# Write an algorithm to find the maximum items that can be packaged for the final group of the list given the conditions above.
+
+# Example - 1 -- arr = [3, 1, 3, 4]. Output: 4. Explanation: Subtract 1 from the first group making the list [2, 1, 3, 4]. Rearrange the list into [1, 2, 3, 4]. The final maximum of items that can be packaged in the last group is 4.
+
+# Example 2 - arr = [1, 1, 1, 1]. Output is 1.
+
+
+def maximum_items_package_automation_for_last(groups: list)-> int:
+    max_val = max(groups)
+    min_val = min(groups)
+    l = len(groups)
+    
+    
+    return min(max_val, min_val + l - 1)
+    
+    
+    
+print(maximum_items_package_automation_for_last([50,1,1,1]))
